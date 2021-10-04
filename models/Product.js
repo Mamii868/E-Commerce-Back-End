faeaa7ef49aa5f8d6987c1c3190c45e5dec55e30
Marc Moten <marcmotenii@gmail.com>
2021-10-03 19:34:33 -0500

    setup Product model

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,7 +10,25 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    
   },
   {
     sequelize,

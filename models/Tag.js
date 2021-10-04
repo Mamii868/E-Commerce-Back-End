daa36f3aa09361831b164375f22e651b1d802beb
Marc Moten <marcmotenii@gmail.com>
2021-10-03 21:46:46 -0500

    added autoIncrement to fix seeding errors

--- models/Tag.js
+++ models/Tag.js
@@ -8,8 +8,9 @@ Tag.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
     tag_name: {
       type: DataTypes.STRING,

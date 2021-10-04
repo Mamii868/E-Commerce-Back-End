b3172bd7d71be70c86b252933b771a3e1d1098da
Marc Moten <marcmotenii@gmail.com>
2021-10-03 20:20:56 -0500

    created get request for all catagories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const productTags = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(productTags);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {

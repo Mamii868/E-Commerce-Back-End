143464b3a3f26e772fd84ff0810fc27d85ffda0b
Marc Moten <marcmotenii@gmail.com>
2021-10-03 21:13:54 -0500

    created update and delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,12 +45,40 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+  const categoryInfo = await Category.update({
+    category_name: req.body.category_name
+  }, {where: {
+    id: req.params.id
+  }})
+  if (!categoryInfo) {
+    res.status(404).json({message: "category does not exist"});
+    return;
+  }
+  res.status(200).json(categoryInfo);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryInfo = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryInfo) {
+      res.status(404).json({message: "category does not exist"});
+      return;
+    }
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
